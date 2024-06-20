2145630e30bc2827baf78286cd7bb8ca715b08e2
Sumit <sumit@clearquote.io>
2024-06-20 18:34:33 +0530

    made changes to train for 50 epochs

--- train_deeptext_ocr.sh
+++ train_deeptext_ocr.sh
@@ -1,12 +1,18 @@
+'''Total images: 1850. 1 epoch = "total_images/batch_size" iterations. 
+    3000 iterations =  50 epochs.
+
+lcd_ocr_v1: 50 epochs, h=120, w=360    
+'''
+
 python3 train.py \
 --exp_name lcd_ocr_v1     \
 --train_data ./lmdb_lcd_dataset/train \
 --valid_data ./lmdb_lcd_dataset/val \
---batch_size 8 \
+--batch_size 32 \
 --select_data "/" \
 --batch_ratio '1' \
---num_iter 1850 \
---valInterval 200 \
+--num_iter 3000 \ 
+--valInterval 58 \
 --augment \
 --log_wandb \
 --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn \
