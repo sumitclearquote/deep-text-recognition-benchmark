acb6f46898f4dddb4132118502d5b0e56a454d88
Sumit <sumit@clearquote.io>
2024-09-20 13:23:38 +0530

    Added sh script for lpscan ocr training

---
+++ train_lpscan_ocr.sh
@@ -0,0 +1,24 @@
+# Total images: 1850. 1 epoch = "total_images/batch_size" iterations. 
+# 3000 iterations =  50 epochs.
+# lcd_ocr_v1: 50 epochs, h=120, w=360  
+
+python train.py \
+--exp_name lcd_ocr_vgg_trial \
+--train_data ./lmdb_lcd_dataset/train \
+--valid_data ./lmdb_lcd_dataset/val \
+--batch_size 32 \
+--select_data "/" \
+--batch_ratio '1' \
+--num_iter 300 \
+--valInterval 60 \
+--augment \
+--log_wandb \
+--Transformation None \
+--FeatureExtraction VGG \
+--SequenceModeling None \
+--Prediction CTC \
+--saved_model saved_models/None-VGG-None-CTC.pth \
+--batch_max_length 6 \
+--imgH 120 \
+--imgW 360 \
+--FT 
