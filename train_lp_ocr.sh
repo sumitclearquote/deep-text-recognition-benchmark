f84c4500c0f228861c22b1dac8bddf83d7a16168
Sumit <sumit@clearquote.io>
2024-09-24 16:01:32 +0530

    Changed result dir in dataset.py

--- train_lp_ocr.sh
+++ train_lp_ocr.sh
@@ -1,6 +1,7 @@
 # Total train images: 5666
 # 1 epoch = "total_images/batch_size" iterations. 
 # 3000 iterations =  50 epochs.
+# lmdb_lp_dataset -> ciaws/msil-temp/lpscan/datasets/lmdb_lp_dataset
 # lp_ocr_v1:  epochs, h=100, w=400  
 # vocab: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-' # Add this in train.py file.
 
