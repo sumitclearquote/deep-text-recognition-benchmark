46324c910b7bf5569e32121515e347ac07d219e5
Sumit <sumit@clearquote.io>
2024-09-24 19:15:33 +0530

    added testing for v1

--- test_lp_ocr.sh
+++ test_lp_ocr.sh
@@ -1,7 +1,13 @@
 python3 test.py \
 --eval_data ./lmdb_lp_dataset/test \
 --batch_size 8 \
---Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn \
---saved_model saved_models/lcd_ocr_v1/best_accuracy.pth --batch_max_length 9 --imgH 100 --imgW 400 \
+--Transformation None
+--FeatureExtraction VGG \
+--SequenceModeling None \
+--Prediction CTC \
+--saved_model saved_models/lcd_ocr_v1/best_accuracy.pth \
+--batch_max_length 9 \
+--imgH 100 \
+--imgW 400 \
 --character '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-' \
 --workers 0
